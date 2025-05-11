ebe2f10bc4a319f6a19adaa9fdd6a03906e5dc5d
iris2598 <iris2598@naver.com>
2025-05-12 04:40:25 +0900

    Home 구분

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import styles from "./Home.module.css";
 const Home = () => {
   return (
     <div className={styles.content}>
-      <div></div>
+      <div>호호호 이게 내 포트폴리오</div>
     </div>
   );
 };
